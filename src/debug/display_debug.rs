cd024b4aba61de00be93ccbbdb0b8da16c1ad0a7
Hedd Schotte <subzerofusion@gmail.com>
2023-12-06 19:34:02 +1000

    omg git ignore

---
+++ src/debug/display_debug.rs
@@ -0,0 +1,43 @@
+use bevy::prelude::*;
+
+use crate::game::keybinds::FloraCommand;
+
+use super::{fps_counter::FPSPlugin, resolution::ResolutionPlugin};
+
+#[derive(Resource)]
+pub struct DebugState {
+    pub is_debug: bool,
+}
+
+impl Default for DebugState {
+    fn default() -> Self {
+        DebugState { is_debug: false }
+    }
+}
+pub struct DisplayDebugPlugin;
+impl Plugin for DisplayDebugPlugin {
+    fn build(&self, app: &mut bevy::prelude::App) {
+        app.init_resource::<DebugState>()
+            .add_plugins((FPSPlugin, ResolutionPlugin))
+            .add_systems(Update, set_debug);
+    }
+}
+
+fn set_debug(mut state: ResMut<DebugState>, input: Res<Input<FloraCommand>>) {
+    if input.just_pressed(FloraCommand::Debug) {
+        state.is_debug = !state.is_debug;
+    }
+}
+
+/// Toggle the FPS counter when pressing F12
+pub fn toggle_visibility<T: Component>(
+    mut q: Query<&mut Visibility, With<T>>,
+    debug: Res<DebugState>,
+) {
+    let mut vis = q.single_mut();
+    *vis = if debug.is_debug {
+        Visibility::Visible
+    } else {
+        Visibility::Hidden
+    }
+}
