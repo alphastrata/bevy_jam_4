ea9c75cfff487153774d4f61135a79a66beaf055
jer <alphastrata@gmail.com>
2023-12-09 22:06:26 +1100

    chore: starting work on creep, break out into diff systems

--- src/creeps/mod.rs
+++ src/creeps/mod.rs
@@ -12,7 +12,7 @@ impl Plugin for CreepPlugin {
 
         app.add_event::<SpawnCreep>();
 
-        app.add_systems(Update, spawn_creep);
+        app.add_systems(Update, initial_creep_spawn);
 
         #[cfg(debug_assertions)]
         app.add_systems(Update, (dbg_send_spawn_creep_on_enter, dbg_count_creeps));
@@ -41,38 +41,54 @@ fn dbg_send_spawn_creep_on_enter(mut spawner: EventWriter<SpawnCreep>, kb: Res<I
     }
 }
 
-fn spawn_creep(
+/// System: Setup
+fn initial_creep_spawn(mut commands: Commands, asset_server: Res<AssetServer>) {
+    (0..1000)
+        .into_iter()
+        .for_each(|_| spawn_creep(&mut commands, &asset_server));
+}
+
+fn spawn_creep(commands: &mut Commands, asset_server: &Res<AssetServer>) {
+    //TODO: random xy spawn at distance from center of map...
+    let mut rng = rand::thread_rng();
+    let eps = 8192.0;
+    let x = rng.gen_range(-eps..=eps);
+    let y = rng.gen_range(-eps..=eps);
+
+    let current = Vec3::new(x, y, 0.0);
+    let target = Vec3::ZERO;
+    let direction = target - current;
+
+    if direction.length() > 240.0 {
+        // Approximately the circle of the 'core'
+        //TODO: Keep a creep count, resource for UI?
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(x, y, 0.10),
+                texture: asset_server.load("textures/creep.png"),
+                //
+                ..default()
+            },
+            Creep,
+            //TODO: creep stats should probs be set by .csv or something?
+            AttackSpeed(10),
+            Health(100),
+            Experience(100),
+        ));
+    }
+}
+
+/// System: Update, listens for the Event<SpawnCreep>, when it does we'll it'll spawn a creep at a random location
+fn spawn_on_trigger(
     mut spawner: EventReader<SpawnCreep>,
     mut commands: Commands,
     asset_server: Res<AssetServer>,
 ) {
-    //TODO: random xy spawn at distance from center of map...
-    spawner.read().for_each(|_spawn_event| {
-        let mut rng = rand::thread_rng();
-        let x = rng.gen_range(-2000.0..=2000.);
-        let y = rng.gen_range(-2000.0..=2000.);
+    spawner
+        .read()
+        .for_each(|_| spawn_creep(&mut commands, &asset_server));
+}
 
-        let current = Vec3::new(x, y, 0.0);
-        let target = Vec3::ZERO;
-        let direction = target - current;
-        dbg!(direction.length());
-
-        if direction.length() > 500.0 {
-            // Approximately the circle of the 'core'
-            //TODO: Keep a creep count, resource for UI?
-            commands.spawn((
-                SpriteBundle {
-                    transform: Transform::from_xyz(x, y, 0.10),
-                    texture: asset_server.load("textures/creep.png"),
-                    //
-                    ..default()
-                },
-                Creep,
-                //TODO: creep stats should probs be set by .csv or something?
-                AttackSpeed(10),
-                Health(100),
-                Experience(100),
-            ));
-        }
-    })
+fn attack_tower() {
+    todo!()
 }
