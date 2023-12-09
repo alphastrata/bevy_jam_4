34fefaa6dc497567cee965fda444201c8f56173c
Omniscient <omniscient.oce@gmail.com>
2023-12-09 23:28:39 +1100

    docs and set a tick rate for damaging trees

--- src/buildings/drain.rs
+++ src/buildings/drain.rs
@@ -1,17 +1,33 @@
 //! A building that drains **all** trees in close proximity.
+//!
+//! **LOGIC:**
+//!
+//! On [SpawnCreep] we recalculate the trees that a particular [DrainTower] can harvest from.
+//! This is because we don't want to do a N^2 every frame.
+//!
+//! Every frame we try and drain a tree
 
 use crate::{creeps::SpawnCreep, game::power::AddBuilding, AppState, Health, Tree};
 use bevy::prelude::*;
 
 use super::BuildingDefinition;
 
+/// Drain damage applied to trees per tick of [GlobalDrainTick]
+const DRAIN_DPT: u32 = 20;
+/// Every *this* many seconds trees get drained
+const DRAIN_TICK_RATE: f32 = 3.0;
+
 #[derive(Component)]
 pub struct DrainRadius(f32);
+
+#[derive(Resource)]
+struct GlobalDrainTick(Timer);
 
 #[derive(Default, Component)]
 pub struct DrainTower {
     trees_in_proximity: Vec<Entity>,
 }
+
 impl BuildingDefinition for DrainTower {
     const SPRITE_PATH: &'static str = "textures/tower.png";
     const BASE_HEALTH: u32 = 100;
@@ -29,7 +45,11 @@ impl BuildingDefinition for DrainTower {
 pub struct DrainTowerPlugin;
 impl Plugin for DrainTowerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(
+        app.insert_resource(GlobalDrainTick(Timer::from_seconds(
+            DRAIN_TICK_RATE,
+            TimerMode::Repeating,
+        )))
+        .add_systems(
             Update,
             (calculate_drainees, drain_closeby_trees).run_if(in_state(AppState::Gameplay)),
         );
@@ -37,7 +57,7 @@ impl Plugin for DrainTowerPlugin {
 }
 
 /// We don't want to calculate the trees in range of a tower every single frame or tick
-/// so instead calculate them when new creeps are spawned.
+/// so this system instead calculates them every time new creeps are spawned.
 fn calculate_drainees(
     mut tower_spawned: EventReader<AddBuilding>,
     mut creep_spawned: EventReader<SpawnCreep>,
@@ -82,14 +102,19 @@ fn calculate_drainees(
 /// NOTE: we don't care about what happens after a tree dies (hp -> zero) here, that should be
 /// handled in other systems.
 fn drain_closeby_trees(
-    q_towers: Query<&DrainTower>,
+    mut timer: ResMut<GlobalDrainTick>,
     mut q_trees: Query<(Entity, &mut Health), With<Tree>>,
+    q_towers: Query<&DrainTower>,
+    time: Res<Time>,
 ) {
-    q_towers.iter().for_each(|tower: &DrainTower| {
-        tower.trees_in_proximity.iter().for_each(|ent| {
-            if let Ok((_tree, mut hp)) = q_trees.get_mut(*ent) {
-                hp.deduct(5);
-            }
+    if timer.0.tick(time.delta()).just_finished() {
+        // for each tower check all the trees in proximity and deduct hp from them.
+        q_towers.iter().for_each(|tower: &DrainTower| {
+            tower.trees_in_proximity.iter().for_each(|ent| {
+                if let Ok((_tree, mut hp)) = q_trees.get_mut(*ent) {
+                    hp.deduct(DRAIN_DPT);
+                }
+            });
         });
-    });
+    }
 }
