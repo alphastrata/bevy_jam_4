4d352c8dc670e6f7c2dcb2e68060091a2db550b4
Omniscient <omniscient.oce@gmail.com>
2023-12-09 21:47:01 +1100

    failed attempt

--- assets/shaders/wind_test.wgsl
+++ assets/shaders/wind_test.wgsl
@@ -1,4 +1,5 @@
 #import bevy_pbr::forward_io::VertexOutput
+#import bevy_sprite::mesh2d_view_bindings globals
 
 // Uniforms
 struct WindSimMaterial {
@@ -8,12 +9,31 @@ struct WindSimMaterial {
 @group(1) @binding(0) var<uniform> material: WindSimMaterial;
 @group(1) @binding(1) var vector_map_texture: texture_2d<f32>;
 @group(1) @binding(2) var vector_map_sampler: sampler;
+@group(1) @binding(3) var particle_texture: texture_2d<f32>;
+@group(1) @binding(4) var particle_sampler: sampler;
 
 @fragment
 fn fragment(mesh: VertexOutput) -> @location(0) vec4<f32> {
-  return textureSample(
+  // Each pixel of the output render texture will correlate to the next position of the
+  // particles.
+
+  let pos = textureSample(
+    particle_texture,
+    particle_sampler,
+    mesh.uv
+  );
+
+  let posv2 = vec2(pos.x, pos.y);
+  let wind = textureSample(
     vector_map_texture,
     vector_map_sampler,
-    mesh.uv);
-  //return material.color;
+    posv2 
+  );
+  let added_wind = normalize(wind) + 0.5;
+
+  let new_pos = pos + sin(globals.time) * added_wind * 0.05;
+  //let new_pos = cross(pos.xyz, wind.xyz);
+  return new_pos;
+
+  //return vec4(vec3(smoothstep(0.1, 0.8, new_pos)), 1.0);
 }
--- examples/quad.rs
+++ examples/quad.rs
@@ -1,11 +1,20 @@
 //! A shader and a material that uses it.
 
+use std::f32::consts::PI;
+
 use bevy::{
     prelude::*,
     reflect::TypePath,
-    render::render_resource::{AsBindGroup, Extent3d, ShaderRef, TextureFormat},
+    render::{
+        camera::RenderTarget,
+        render_resource::{
+            AsBindGroup, Extent3d, ShaderRef, TextureDimension, TextureFormat, TextureUsages,
+        },
+        view::RenderLayers,
+    },
     sprite::{Material2d, Material2dPlugin, MaterialMesh2dBundle, Mesh2dHandle},
 };
+use image::{DynamicImage, GenericImage, Rgba};
 
 fn main() {
     App::new()
@@ -14,6 +23,7 @@ fn main() {
             Material2dPlugin::<WindSimMaterial>::default(),
         ))
         .add_systems(Startup, setup)
+        .add_systems(Update, dummy_update)
         .run();
 }
 
@@ -25,9 +35,9 @@ pub struct WindSimMaterial {
     #[texture(1)]
     #[sampler(2)]
     vector_map: Option<Handle<Image>>,
-    // #[texture(3)]
-    // #[sampler(4)]
-    // particle_positions: Option<Handle<Image>>,
+    #[texture(3)]
+    #[sampler(4)]
+    particle_positions: Option<Handle<Image>>,
 }
 impl Material2d for WindSimMaterial {
     fn fragment_shader() -> ShaderRef {
@@ -35,13 +45,23 @@ impl Material2d for WindSimMaterial {
     }
 }
 
+fn dummy_update(query: Query<&MyLittleImage>, images: Res<Assets<Image>>) {
+    let i: &MyLittleImage = query.single();
+
+    let image: &Image = images.get(&i.0).unwrap();
+    println!("{:?}", &image.data);
+}
+
+#[derive(Component)]
+struct MyLittleImage(Handle<Image>);
+
 /// set up a simple 3D scene
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<WindSimMaterial>>,
+    mut wind_mat: ResMut<Assets<WindSimMaterial>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
     mut images: ResMut<Assets<Image>>,
-    _asset_server: Res<AssetServer>,
 ) {
     let size = Extent3d {
         width: 256,
@@ -50,42 +70,125 @@ fn setup(
     };
 
     // Create a texture for the Wind Vectors (replace this with something non uniform colour)
-    let wind_image = Image::new_fill(
+    let _wind_image = Image::new_fill(
         size,
         bevy::render::render_resource::TextureDimension::D2,
-        &[125, 0, 255, 255],
+        &[125, 0, 255, 1],
         TextureFormat::Rgba8Unorm,
     );
+
+    let mut wind_image = DynamicImage::new_rgb8(256, 256);
+    for x in 0..256 {
+        for y in 0..256 {
+            wind_image.put_pixel(x, y, Rgba([x as u8, y as u8, 0, 1]));
+        }
+    }
+    let wind_image = Image::from_dynamic(wind_image, false);
     let wind_handle = images.add(wind_image);
 
     // Create a texture to render the new positions to.
+    let mut start_pos_image = Image::new_fill(
+        size,
+        TextureDimension::D2,
+        &[255, 0, 255, 255],
+        TextureFormat::Bgra8UnormSrgb,
+    );
+    start_pos_image.texture_descriptor.usage =
+        TextureUsages::TEXTURE_BINDING | TextureUsages::COPY_DST | TextureUsages::RENDER_ATTACHMENT;
+
+    let mut wind_image = DynamicImage::new_rgb8(256, 256);
+    for x in 0..256 {
+        for y in 0..256 {
+            wind_image.put_pixel(x, y, Rgba([x as u8, y as u8, 0, 1]));
+        }
+    }
+    start_pos_image.texture_descriptor.usage =
+        TextureUsages::TEXTURE_BINDING | TextureUsages::COPY_DST | TextureUsages::RENDER_ATTACHMENT;
+
+    /*
+    Positions
+    [  (x1, y1) , (), () ]
+
+    Wind vectors
+    [
+        ......
+        ......
+        ......
+        ......
+    ]
+    */
+
+    let particle_handle = images.add(start_pos_image);
+
+    // First pass
+    let first_pass_layer = RenderLayers::layer(1);
 
     let quad = meshes.add(Mesh::from(shape::Quad::new(Vec2::new(512.0, 512.0))));
-
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: Mesh2dHandle(quad),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        material: materials.add(WindSimMaterial {
-            color: Color::BLUE,
-            vector_map: Some(wind_handle),
-        }),
-        ..default()
-    });
-
+    // mesh
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: Mesh2dHandle(quad),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            material: wind_mat.add(WindSimMaterial {
+                color: Color::rgb(0.1, 0.1, 0.1),
+                vector_map: Some(wind_handle),
+                particle_positions: Some(particle_handle.clone()),
+            }),
+            ..default()
+        },
+        first_pass_layer,
+        MyLittleImage(particle_handle.clone()),
+    ));
     // camera
-    commands.spawn(Camera2dBundle {
-        transform: Transform::from_xyz(0.0, 0.0, 1.0).looking_at(Vec3::ZERO, Vec3::Y),
+    commands.spawn((
+        Camera2dBundle {
+            camera: Camera {
+                order: -1,
+                target: RenderTarget::Image(particle_handle.clone()),
+                // target: RenderTarget::TextureView(()),
+                ..default()
+            },
+            transform: Transform::from_xyz(0.0, 0.0, 1.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        first_pass_layer,
+    ));
+
+    // Second pass
+    // mesh
+    let cube_size = 4.0;
+    let cube_handle = meshes.add(Mesh::from(shape::Box::new(cube_size, cube_size, cube_size)));
+
+    // This material has the texture that has been rendered.
+    let material_handle = materials.add(StandardMaterial {
+        base_color_texture: Some(particle_handle),
+        reflectance: 0.02,
+        unlit: false,
         ..default()
     });
-}
 
-// This is the struct that will be passed to your shader
-#[derive(Asset, TypePath, AsBindGroup, Debug, Clone)]
-pub struct CustomMaterial {
-    #[uniform(0)]
-    color: Color,
-    #[texture(1)]
-    #[sampler(2)]
-    color_texture: Option<Handle<Image>>,
-    alpha_mode: AlphaMode,
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+
+    // Main pass cube, with material containing the rendered first pass texture.
+    commands.spawn(PbrBundle {
+        mesh: cube_handle,
+        material: material_handle,
+        transform: Transform::from_xyz(0.0, 0.0, 1.5)
+            .with_rotation(Quat::from_rotation_x(-PI / 5.0)),
+        ..default()
+    });
+
+    // The main pass camera.
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0.0, 0.0, 15.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
