4cf2a1c8c844cd2db05e88c2ac1a0ede1a18181d
Omniscient <omniscient.oce@gmail.com>
2023-12-11 01:01:49 +1100

    final day jank. deduplicate system with different name to resolve power dependencies

--- src/game/power.rs
+++ src/game/power.rs
@@ -38,8 +38,12 @@ impl Plugin for PowerPlugin {
         app.add_event::<AddBuilding>();
         app.add_systems(OnEnter(AppState::Gameplay), create_core);
         app.add_systems(
-            PostUpdate,
+            Update,
             (update_powered_unpowered).run_if(in_state(AppState::Gameplay)),
+        )
+        .add_systems(
+            PostUpdate,
+            (update_powered_unpowered2).run_if(in_state(AppState::Gameplay)),
         )
         .add_systems(
             Update,
@@ -55,6 +59,53 @@ fn create_core(mut commands: Commands, asset_server: Res<AssetServer>) {
 
 /// Updates the set of towers that are powered or unpowered
 fn update_powered_unpowered(
+    mut commands: Commands,
+    mut update_trigger: EventReader<AddBuilding>,
+    building_query: Query<(Entity, &Transform), With<RequiresPower>>,
+    supply_query: Query<(Entity, &SupplyRadius, &Transform), With<IsPowered>>,
+) {
+    if update_trigger.read().last().is_some() {
+        // TODO: IF PERFORMANCE DIE, QUADTREE GO HERE.
+
+        // for every building check that its powered by at least one building
+        // O(n^2) ish (Power supplying buildings * power drawing buildings)
+        building_query.iter().for_each(|(entity, drawer_tf)| {
+            let is_powered = supply_query.iter().try_fold(
+                None,
+                |_acc: Option<f32>, (supply_entity, radius, supply_tf)| {
+                    let drawer_pos = drawer_tf.translation;
+                    let supply_pos = supply_tf.translation;
+
+                    let distance = ((drawer_pos.x - supply_pos.x) * (drawer_pos.x - supply_pos.x)
+                        + (drawer_pos.y - supply_pos.y) * (drawer_pos.y - supply_pos.y))
+                        .sqrt();
+                    if distance < radius.0 {
+                        ControlFlow::Break(Some(supply_entity))
+                    } else {
+                        ControlFlow::Continue(None)
+                    }
+                },
+            );
+            match is_powered {
+                ControlFlow::Break(is_powered) => {
+                    if let Some(supply_ent) = is_powered {
+                        // Only buildings other than this building itself can power it
+                        if supply_ent != entity {
+                            commands.entity(entity).insert(IsPowered);
+                        } else {
+                            commands.entity(entity).remove::<IsPowered>();
+                        }
+                    }
+                }
+                ControlFlow::Continue(_) => {
+                    commands.entity(entity).remove::<IsPowered>();
+                }
+            }
+        });
+    }
+}
+/// Updates the set of towers that are powered or unpowered
+fn update_powered_unpowered2(
     mut commands: Commands,
     mut update_trigger: EventReader<AddBuilding>,
     building_query: Query<(Entity, &Transform), With<RequiresPower>>,
